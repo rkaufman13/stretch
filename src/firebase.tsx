aaa59274b81d90d4267733ea9455758c08642bed
Avery <avery@averyf.dev>
2024-11-17 06:44:56 -0500

    add script tag to index.html

--- src/firebase.tsx
+++ src/firebase.tsx
@@ -1,13 +1,12 @@
 import { initializeApp } from "firebase/app";
 import {
-  getAuth,
   connectAuthEmulator,
-  GoogleAuthProvider,
-  EmailAuthProvider,
+  getAuth,
+  GoogleAuthProvider
 } from "firebase/auth";
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyBVCyNLMhmnbp7CCbM3ghq_wuiEp0ZFU-c",
+  apiKey: "AIzaSyBVCyNLMhmnbp7CCbM3ghq_wuiEp0ZFU-c", // lmao
   authDomain: "stretch-5cf3f.firebaseapp.com",
   databaseURL: "https://stretch-5cf3f-default-rtdb.firebaseio.com",
   projectId: "stretch-5cf3f",
@@ -23,7 +22,7 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth();
 
 export const provider = new GoogleAuthProvider();
-//export const provider = new EmailAuthProvider();
+// export const provider = new EmailAuthProvider();
 if (process.env.NODE_ENV === "development") {
   connectAuthEmulator(auth, "http://127.0.0.1:9099");
 }
